6b3443c095f48948ad3f9bca6e64c554b2f56896
tpaulben10-alt <tpaulbenedictsandy@gmail.com>
2026-04-14 12:23:11 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const app = express();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+app.get("/", (req, res) => {
+  db.query("SELECT NOW()", (err, result) => {
+    if(err) throw err;
+    res.send("Database Connected Successfully: " + result[0]["NOW()"]);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
